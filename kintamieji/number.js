5da917cb85d9a65a9aacb4d7dd4875a28e7bef7a
modestas <modestas.juska@gmail.com>
2023-09-20 10:31:51 +0300

    Gitignore

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -2,6 +2,30 @@ const amzius = 99;
 let pinigine = 0;
 var minusas = -7.56;
 
+
+
+const n1 = 7;
+const n2 = 5;
+
+const sum = n1 + n2;
+// 12 = 7 + 5
+
+
+
+const div = n1 / n2;
+// 1,4 = 7 / 5
+
+
 console.log(amzius);
+console.log(sum);
+console.log(div);
 
 
+const complex = (2 + 2) * 2;
+console.log(complex);
+
+const exp1 = 2 + 2;
+const exp2 = 2 * 2;
+const exp3 = 2 ** 2;
+
+console.log(exp1, exp2, exp3);
