39faed4460b9f08913eeb6ef7d16255797fd0e6b
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-09-23 21:48:45 +0300

    Infinity and liekana

--- kintamieji/number.js
+++ kintamieji/number.js
@@ -28,5 +28,30 @@ const exp1 = 2 + 2;
 const exp2 = 2 * 2;
 const exp3 = 2 ** 2;
 
+const exp4 = 3 ** 3;
+console.log(exp4);
+
 console.log(26-10, exp2, exp3);
 console.log(exp2 + exp3);
+
+const begalybe = Infinity;
+const minusbegalybe = -Infinity;
+
+const x = 5 / 0;
+console.log(x);
+
+const g = -5 / 0;
+console.log(g);
+
+// const n = NaN (arba klaida)
+
+const n = NaN;
+console.log(n, NaN);
+
+// Liekana (dalis, kuri lieka po pirmo dalybos veiksmo)
+const liekana = 7 % 5;
+console.log(liekana);
+
+const liekana1 = 65 % 13;
+console.log(liekana1);
+
--- kintamieji/string.js
+++ kintamieji/string.js
@@ -1,3 +1,9 @@
 //console.clear  
 const amzius = 50;
-console.log ("zmogus, kurio amzius", amzius);
+console.log ('zmogus, kurio amzius', amzius);
+
+const txt1 = "tekstas 1";
+console.log(txt1);
+
+const txt2 = "tekstas 2";
+console.log(txt2);
