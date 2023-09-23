4b894f9681cb107cb0ce19ec029b0416666b0c9b
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-09-23 22:40:40 +0300

    new and return

--- kintamieji/string.js
+++ kintamieji/string.js
@@ -7,3 +7,22 @@ console.log(txt1);
 
 const txt2 = "tekstas 2";
 console.log(txt2);
+
+console.clear();
+
+// vienguba kabute (') ->\ pries simboli reiskia, kad bus simbolis spausdinamas
+const kabute1 = "vienguba kabute (').";
+console.log(kabute1);
+
+// vienguba ir dviguba kabutes
+const kabute2 = "vienguba kabute (') ir dvigubos kabutes (\")";
+console.log(kabute2);
+
+const back = "\\";
+console.log(back);
+
+// \n - new line, \r - return
+const formatuotastekstas = "Labas \n\rrytas, \n\rLietuva!";
+console.log(formatuotastekstas);
+
+
