6eb986f0fc628222fb317858efca4eb8d3b3e3c4
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-09-25 23:05:45 +0300

    Key value

--- kintamieji/object.js
+++ kintamieji/object.js
@@ -6,11 +6,15 @@ Objektas, daiktas, dalykas.
 
 // vartotojas: vardas, amzius, ar vedes
 
-// key walue
+// key value
+
 const jonas = {
-    name: 'Jonas',
-    age: 99,
-    ismarried: true,
+name: 'Jonas',
+age: 99,
+ismarried: true,
 };
 
-console.log(jonas[]);
+console.log(jonas);
+console.log(jonas['name'], jonas.name);
+console.log(jonas['age'], jonas.age);
+console.log(jonas['ismarried'], jonas.ismarried);
