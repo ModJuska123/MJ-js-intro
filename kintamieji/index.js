01217fe9530353b5155bff78c870978b53a08f77
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-09-26 22:15:12 +0300

    Description

---
+++ kintamieji/index.js
@@ -0,0 +1,14 @@
+/*
+Kintamieji
+
+Primityvūs duomenų tipai:
+- number
+- srting
+- boolean (true/false)
+- undefined
+- null
+
+Kompleksiniai duomenų tipai:
+- array (masyvas/kolekcija/sąrašas/ listas/matrica/arėjas)
+- object (kompleksinei informacijai aprašyti)
+*/
