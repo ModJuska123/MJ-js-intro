9c632b659219c4199ea9d42aedabe1a4080fe6cb
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-10-02 21:25:15 +0300

    update

---
+++ method/string.js
@@ -0,0 +1,61 @@
+
+
+// isveda indexo simboli sklaiustuose
+
+
+
+// isveda simbolio koda
+
+
+// sujungia stringus
+
+
+// Sujungia simbolius
+
+/*let str3 = 'Hello';
+const str4 = 'World';
+
+str3 += ' ';
+str3 += str4;
+console.log(str3);
+
+let str5 = 'Hello';
+str5 += ' ' + str4;
+console.log(str5);
+
+console.clear();
+
+const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+
+const start = 'Abecele: ';
+
+let a1 = '';
+a1 += start;
+a1 += abc[0];
+a1 += abc[1];
+a1 += abc[2];
+a1 += abc[3];
+a1 += abc[4];
+a1 += abc[5];
+console.log(a1);
+
+let a2 = start + abc[0] + abc[1] + abc[2] + abc[3] + abc[4] + abc[5];
+console.log(a2);
+
+let a3 = start;
+
+for (let i = 0; i < abc.length; i++) {
+    a3 += abc[i];
+}
+
+console.log(a3);
+
+const a4 = start.concat(...abc);
+console.log(a4);
+
+const a5 = start.concat(abc[0], abc[1], abc[2], abc[3], abc[4], abc[5]);
+console.log(a5);
+*?/
+
+// ar yra atsakyme reiksme
+
