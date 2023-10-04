fbace23720a0c3d8ec507c1cb3bd6efab5faa39c
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-10-04 17:51:39 +0300

    Update

--- method/string.js
+++ method/string.js
@@ -37,3 +37,75 @@ kiek += 1} else {kiek += 0}
 ;
 }
 console.log(`${kiek} a zodyje: ${labas}`)
+
+console.clear();
+
+//-> .at() iskvietimas indekso
+
+console.log('labas'[0], 'labas'.at(-1));
+console.log('labas'[1], 'labas'.at(-2));
+console.log('labas'[2], 'labas'.at(-3));
+console.log('labas'[3], 'labas'.at(-4));
+console.log('labas'[4], 'labas'.at(-5));
+
+//-> ..charCodeAt() iskvietimas simbolio kodo
+
+console.log('labas'.charCodeAt(0));
+
+//-> ..concat() sujungia sringus be tarpo
+
+const str1 = 'Hello';
+const str2 = 'world';
+console.log(str1.concat(' ', str2));
+
+const abc = ['a', 'b', 'c', 'd', 'e', 'f'];
+const startas = 'Abecele:';
+const a4 = startas.concat(...abc);
+console.log(a4);
+
+//-> .includes() tikrina Boolean stringe yra simbolis
+
+console.log('labas'.includes('k'));
+
+//-> .indexOf() sujungia sringus be tarpo
+
+console.log('labas'.indexOf('l'));
+
+//-> .repeat() pakartoja reiksme n kartu
+
+console.log('labas'.repeat('6'));
+
+
+//-> .replace() pakeicia reiksme is a i b
+
+console.log('labas'.replace('a', 'b'));
+
+//-> .replaceAll() pakeicia visas reiksme is a i b
+
+console.log('labas'.replaceAll('a', 'b'));
+
+//-> .slice() atvaizduoja simbolius pagal nurodyt1 indexa
+
+console.log('labas'.slice(0, 2));
+
+//-> .split() sukarpo pagal skliaustuose nurodyta simboli
+
+console.log('labas rytas suraitytas'.split(' '));
+
+//-> ..startsWith() 
+console.log('labas rytas suraitytas'.startsWith('r'));
+
+//-> .split() 
+console.log('labas rytas suraitytas'.endsWith('s'));
+
+
+//-> .toUpperCase() i didziasias pakeisti raides
+console.log('pasaka'.toUpperCase());
+
+//-> .toLowerCase() i didziasias pakeisti raides
+console.log('PASAKA'.toLowerCase());
+
+//-> .trim() nukarpo tuscius sonus
+console.log('   Labas    rytas     '.trim());
+
+
