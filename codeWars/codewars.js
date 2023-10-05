4852e971b3c00ad6ef7cfba7599707201cfbfce8
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-10-05 23:29:01 +0300

    Update

--- codeWars/codewars.js
+++ codeWars/codewars.js
@@ -38,3 +38,37 @@ function replaceDots(str){
   return replacement;
 } 
 console.log(replaceDots('st.replace'));
+
+// Get ASCII value of a character.
+
+function getASCII(index) {
+ const indexASCII = index.charCodeAt();
+ return indexASCII;
+}
+console.log(getASCII('c'));
+
+// In this Kata we are passing a number (n) into a function.
+// Your code will determine if the number passed is even (or not).
+// The function needs to return either a true or false.
+// Numbers may be positive or negative, integers or floats.
+// Floats with decimal part non equal to zero are considered UNeven for this kata.
+// function testEven(n) {
+
+function testEven(n) {
+  if(n % 2 === 0)
+  {return `true`} else 
+  {return `false`};
+}
+console.log(testEven(0));
+console.log(testEven(1));
+
+
+// Your task is to write a function that takes a String and 
+// returns an Array/list with the length of each word added to each element .
+// Note: String will have at least one element; words will 
+// always be separated by a space.
+
+function addLength(str2) {
+const words = str2.split(" ");
+return words.map(str2 => `${str2} ${str2.length}`);}
+console.log(addLength('Hello World'));
