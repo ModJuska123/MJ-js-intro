a6af0e6d4babf1dd908a6e1951d11c99063033a9
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-10-10 20:48:20 +0300

    Update

--- codeWars/codewars.js
+++ codeWars/codewars.js
@@ -299,3 +299,22 @@ console.log(kmi(markoUgis, markoSvoris));
 const palyginimas = markoKMI1 > jonoKMI1 ? 
 console.log(`„markHigherBMI - true`) : 
 console.log(`„markHigherBMI - false`);
+
+// Friday chalenge 2:
+
+// Evenas nori sukurti labai paprastą arbatpinigių skaičiuotuvą, kai jis eina valgyti į 
+// restoraną. Jo šalyje įprasta duoti 15 % arbatpinigių, jei sąskaitos vertė yra nuo 50 iki
+// 300. 
+// Jei vertė skiriasi, arbatpinigiai yra 20%.
+// Jūsų užduotys:
+// Apskaičiuokite arbatpinigių, priklausomai nuo sąskaitos vertės. Sukurkite kintamąjį pavadinimu „Tip“.
+// Neleidžiama naudoti if/else teiginio  (Jei jums taip lengviau, galite
+// pradėti nuo if/else teiginio, tada pabandykite jį konvertuoti į ternery
+// operator!)
+// Į konsole atspausdinkite eilutę, kurioje yra sąskaitos vertė, arbatpinigiai ir galutinė vertė
+// (sąskaita + arbatpinigiai). Pavyzdys: „Sąskaita buvo 275, arbatpinigiai 41,25, o bendra vertė
+// 316,25"
+// Bandymo duomenys:
+// § 1 duomenys: patikrinkite sąskaitos reikšmes 275, 40 ir 430
+
+
