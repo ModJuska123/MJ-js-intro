96cfe7288ae487cf0ddf3c0674617a8500e70b09
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-10 10:09:15 +0200

    Method;array-loops

--- method/array-loops.js
+++ method/array-loops.js
@@ -8,18 +8,60 @@ function doubleOrNothing2(number) {
 const doubleMarkMap2 = marks.map(doubleOrNothing2);
 console.log('MAP', doubleMarkMap2);
 
- //su rodykline funkcija:
+ // .map - su rodykline funkcija:
 
 const doubleMarkMap3 = marks.map(n => n * 2);
 console.log('MAP2', doubleMarkMap3);
 
-const badMarks = [10, 12, 2, NaN, 3.14, 'Blogas pazymys'];
+const badMarks = [10, 12, 2, NaN, 3.14, 'Blogas pazymys', 3, 5, 8];
 function arSkaicius(number) {
-    if (typeof number === 'string') {return false}
+    if (typeof number === 'string') {
+        return false
+    };
+    if (isNaN(number)) {
+        return false
+    };
+    if (number % 1 !== 0) {
+        return false
+    };
+    return true;
     
 };
-const goodMarksOnly = badMarks.filter();
+const goodMarksOnly = badMarks.filter(arSkaicius);
 console.log(goodMarksOnly);
 
+// .map() su filtrais ir rodykline funkcija
+console.log('.map() su filtrais su rodykline funkcija-------');
+const goodMarksOnly2 = badMarks
+    .filter(n => typeof n === 'number')
+    .filter(n => isFinite(n))
+    .filter(n => n > 0)
+    .filter(n => n < 11)
+    .filter(n =>Number.isInteger(n));
+
+    console.log(goodMarksOnly2);
+
+// .map() su filtrais ir rodykline funkcija (sutrumpinus koda):
+
+const goodMarksOnly4 = badMarks
+    .filter(n => Number.isInteger(n) && isFinite(n) && n > 0 && n < 11 && Number.isInteger(n))
+console.log(goodMarksOnly4);
+
+
+
+
+
+
+
+
+
+// .map() metodas grazina nauja masyva per funkcija
+
+// const marks1 = [10, 2, 8, 5];
+
+// const doubleMarkMap4 = marks1.map(n => n * 2);
+
+// console.log(doubleMarkMap4);
+// console.log('labas');
 
 
