852c768cb816e363d4c65d5b003e2e37bcca89fb
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2023-11-16 14:37:57 +0200

    square root

--- codeWars/codewars.js
+++ codeWars/codewars.js
@@ -366,3 +366,30 @@ const PremierLeagueStandings = resetLeagueStandings(previousStandings);
 
 // Output the new standings
 console.log(PremierLeagueStandings);
+
+console.log('------------------');
+// to square root:
+//Write a method, that will get an integer array 
+// as parameter and will process every number from this 
+// array.
+
+// Return a new array with processing every number 
+// of the input-array like this:
+
+// If the number has an integer square root, 
+// take this, otherwise square the number.
+
+// Example
+// [4,3,9,7,2,1] -> [2,9,3,49,4,1]
+// Notes
+// The input array will always contain only 
+// positive numbers, and will never be empty or null.
+//https://www.codewars.com/kata/57f6ad55cca6e045d2000627/train/javascript
+
+// const array = [4, 8];
+// function squareOrSquareRoot(array) {
+//     return array Math.sqrt();  
+   
+//   }
+//   console.log(array); NEBAIGIAU
+  
