78d4ccbc3956d66165d119d34dcd907b9ce5578b
ModJuska123 <142696870+ModJuska123@users.noreply.github.com>
2024-03-30 00:33:19 +0200

    update

--- codeWars/codewars.js
+++ codeWars/codewars.js
@@ -1554,3 +1554,69 @@ function removeEveryOther(arr) {
 console.log(removeEveryOther(["Keep", "Remove", "Keep", "Remove", "Keep"]));
 
 console.log("labas");
+
+//fizzBuz
+// let result111 = function fizzBuzz(n) {
+// for (let i = 1; i <= n; i++) {
+//   if (i % 15 === 0) {
+//       console.log("FizzBuzz");
+//   } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//   } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//   } else {
+//       console.log(i);
+//   }
+// }
+// };
+// console.log(result111);
+
+
+// let fizzBuzz = function (n) { 
+  
+//   const arr = ""; 
+//   for (i = 1; i <= n; i++) { 
+//       if (i % 15 === 0) arr.push("FizzBuzz") 
+//       else if (i % 3 === 0) arr.push("Fizz") 
+//       else if (i % 5 === 0) arr.push("Buzz") 
+//       else arr.push(i.toString()) 
+//   } 
+//   return arr 
+
+// }; 
+// console.log(fizzBuzz(15))
+
+// const n = 15;
+// let result111 = function fizzBuzz(n) {
+//   for (let i = 1; i <= n; i++) {
+//     if (i % 15 === 0) {
+//       console.log("FizzBuzz");
+//     } else if (i % 3 === 0) {
+//       console.log("Fizz");
+//     } else if (i % 5 === 0) {
+//       console.log("Buzz");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
+let result111 = function fizzBuzz(n) {
+  for (let i = 1; i <= n; i++) {
+    if (i % 15 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+};
+
+result111(15); // Change 20 to whatever number you want to test
+
+
+// ....new chalenge:
+
+
